cf9b8ae3fed9ad45b365b711d4b1d77223e7fae7
Antonio Franco <ideascope.ai@gmail.com>
2025-08-20 22:09:36 -0400

    Fix Vite build rollup input configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/main.tsx'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
